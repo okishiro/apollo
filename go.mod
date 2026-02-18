66b3b4a344bc06ba7f361a6b2f69c3778725ceec
iwaili <singh.harshit2703@gmail.com>
2026-02-18 11:29:05 +0530

    GOTH is messy

--- go.mod
+++ go.mod
@@ -3,18 +3,37 @@ module github.com/okishiro/pidgey
 go 1.24.4
 
 require (
+	github.com/a-h/templ v0.3.977
+	github.com/go-playground/validator/v10 v10.30.1
+	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/mattn/go-sqlite3 v1.14.33
+)
+
+require (
 	github.com/BurntSushi/toml v1.6.0 // indirect
+	github.com/a-h/parse v0.0.0-20250122154542-74294addb73e // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cli/browser v1.3.0 // indirect
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.12 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.30.1 // indirect
-	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/mattn/go-sqlite3 v1.14.33 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/natefinch/atomic v1.0.1 // indirect
 	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/mod v0.30.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/sys v0.39.0 // indirect
 	golang.org/x/text v0.32.0 // indirect
+	golang.org/x/tools v0.39.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
+
+tool github.com/a-h/templ/cmd/templ
--- internal/http/handlers/hello/one.go
+++ internal/http/handlers/hello/one.go
@@ -2,56 +2,64 @@ package one
 
 import (
 	"encoding/json"
-	"errors"
-	"io"
 	"log"
 	"log/slog"
 	"net/http"
 	"strconv"
 	"time"
 
-	"github.com/go-playground/validator/v10"
 	Storage "github.com/okishiro/pidgey/internal/storage"
 	types "github.com/okishiro/pidgey/internal/types"
+	"github.com/okishiro/pidgey/internal/ui"
 	"github.com/okishiro/pidgey/internal/utils/response"
 )
 
-func CreateMovie(datab Storage.Store) http.HandlerFunc { //pass the interface
+func CreateMovie(datab Storage.Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		var movieTitle, comment string
 
-		var recieved types.Movie
+		// 1. Get accountname from the URL {name}
 		accountname := r.PathValue("name")
-		if accountname == "" {
-			http.Error(w, "name is required", http.StatusBadRequest)
-			return
-		}
-		err := json.NewDecoder(r.Body).Decode(&recieved)
-		if errors.Is(err, io.EOF) {
-			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
-			return
+
+		// 2. Decide if we are reading JSON or FORM data
+		if r.Header.Get("Content-Type") == "application/x-www-form-urlencoded" {
+			// Data from your GOTH frontend
+			movieTitle = r.FormValue("name")
+			comment = r.FormValue("comment")
+		} else {
+			// Data from your JSON stress tests
+			var recieved types.Movie
+			if err := json.NewDecoder(r.Body).Decode(&recieved); err == nil {
+				movieTitle = recieved.Name
+				comment = recieved.Comment
+			}
 		}
 
-		if err := validator.New().Struct(recieved); err != nil {
-			validateError := err.(validator.ValidationErrors)
-			response.WriteJson(w, http.StatusBadRequest, response.ValidationErrors(validateError))
-			return
-		}
-
+		// 3. Generate the 3rd argument (time)
 		today := time.Now().UTC().Truncate(24 * time.Hour)
+
+		// 4. Pass the 4 arguments to your existing DB logic
 		lastid, err := datab.CreateMovieEntry(
-			accountname,
-			recieved.Name,
-			today,
-			recieved.Comment,
+			accountname, // Arg 1
+			movieTitle,  // Arg 2
+			today,       // Arg 3
+			comment,     // Arg 4
 		)
+
 		if err != nil {
 			slog.Error("Database Error", "details", err.Error())
-			response.WriteJson(w, http.StatusInternalServerError, err.Error())
+			http.Error(w, "DB Fail", 500)
 			return
 		}
 
-		slog.Info("creating an entry for %s with id - %v", accountname, lastid)
-		response.WriteJson(w, http.StatusCreated, "")
+		// 5. Response logic
+		if r.Header.Get("HX-Request") == "true" {
+			// For the frontend: Send back a single row
+			ui.MovieRow(movieTitle, accountname).Render(r.Context(), w)
+		} else {
+			// For the API: Send back JSON
+			response.WriteJson(w, http.StatusCreated, map[string]int64{"id": lastid})
+		}
 	}
 }
 
